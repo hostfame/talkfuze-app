aefcdbe55e00b1f94684204ed74d4a9f1280e2bc
Imran <info@hostfame.com>
2026-05-22 19:13:05 +0600

    chore: cleanup temp file

--- src/actions/call-logging.ts
+++
@@ -1,3 +0,0 @@
-export async function logCallToConversation(orgId: string, conversationId: string, agentName: string, duration: number, status: string) {
-  // We will write this later, just planning
-}
